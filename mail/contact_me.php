c2ad3be2333dadd9c7fcfe6b98297fb8c3a7dbc8
Matt Iakhno <matt.iakhno@gmail.com>
2018-12-08 17:30:43 -0800

    Commit

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -10,6 +10,8 @@ if(empty($_POST['name'])      ||
    return false;
    }
 
+echo "<script>console.log( 'Sent email!' );</script>";
+
 $name = strip_tags(htmlspecialchars($_POST['name']));
 $email_address = strip_tags(htmlspecialchars($_POST['email']));
 $phone = strip_tags(htmlspecialchars($_POST['phone']));
